271b83af322650d944639da0874ef249c85ec5ef
home <home@MacBook-Pro.local>
2022-08-20 19:43:11 -0300

    Primeiro encontro - criando o servidor

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express') //importando o express
-
-const app = express() //executar o express:
-
-const PORT = 3000 //porta
+const app = express() //executar o express
+const PORT = 3000
 
 //criando rota get
-app.get('/', function(req, res) {
-    res.send('<h1>Bem-vindas!!!</h1>')
+app.get('/', function (req, res) {
+  res.send('<h1>Bem-vindas!!!</h1>')
 })
 
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`)) //iniciando o servidor
+//iniciando o servidor
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
