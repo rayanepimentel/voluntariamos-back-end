cd18c63d442366000878227ca12800b8618e662a
Rayane Pimentel <radharayg@gmail.com>
2022-10-23 17:40:41 -0300

    feat: router event put

--- src/routers/eventRouter.js
+++ src/routers/eventRouter.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const controller = require('../controllers/eventController')
 
 router.post('/criar', controller.criarEvento)
-router.patch('/editar/:id', controller.editarEvento)
+router.put('/editar/:id', controller.editarEvento)
 router.get('/listar', controller.exibirEventos)
 router.delete('/excluir/:id/:idEvento', controller.excluirEvento)
 
