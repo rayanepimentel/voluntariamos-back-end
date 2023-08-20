9f672f740dddb6d90f74cde3cb44674bec23260d
Rayane Pimentel <37915359+rayanepimentel@users.noreply.github.com>
2023-08-19 22:04:47 -0300

    teste scan server.js

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const app = require('./src/app')
 const PORT = process.env.PORT
 
 //iniciando o servidor
-app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
+app.listen(PORT, () => console.log(`Rodando na porta ${PORT}!!!`))
